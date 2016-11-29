b0d9750019429a51da6ca1deb24c07edef73f1a2
christine2actes <christine2actes@yandex.com>
2016-11-29 10:04:10 +0100

    29/11

--- app/Views/default/home.php
+++ app/Views/default/home.php
@@ -1,7 +1,7 @@
 <?php $this->layout('layout', ['title' => 'Accueil de t\'chat']) ?>
 
 <?php $this->start('main_content') ?>
-	<h2>Bonjour <?php echo $_SESSION['user']['pseudo']; ?> </h2>
+	<h2>Bonjour <?php echo !empty($_SESSION) ? $_SESSION['user']['pseudo'] : ' ! '; ?> </h2>
 	<h3>Bienvenue sur T'chat !</h3>
 	<p>Le super chat de WF3 !!!</p>
 <?php $this->stop('main_content') ?>
